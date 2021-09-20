cff45655266c351107b33875ffd7e9095b8e647d
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-09-20 17:37:05 +0600

    Sign up and Login Page added

---
+++ src/pages/Login.jsx
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+            rgba(255, 255, 255, 0.5),
+            rgba(255, 255, 255, 0.5)
+        ),
+        url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+            center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Wrapper = styled.div`
+    width: 20%;
+    padding: 20px;
+    background-color: white;
+`;
+
+const Title = styled.h1`
+    display: flex;
+    font-size: 28px;
+    font-weight: 250;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    padding: 10px;
+    margin: 10px 0px;
+`;
+
+const Button = styled.button`
+    width: 35%;
+    border: none;
+    padding: 15px 20px;
+    cursor: pointer;
+    background-color: teal;
+    color: white;
+    margin-bottom: 10px;
+    &:hover {
+        background-color: #198d8d;
+        color: white;
+    }
+`;
+const Link = styled.a`
+    margin: 5px 0px;
+    font-size: 14px;
+    cursor: pointer;
+    text-decoration: underline;
+`;
+
+const Login = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>SIGN IN</Title>
+                <Form>
+                    <Input placeholder="username" />
+                    <Input placeholder="password" />
+                    <Button>LOGIN</Button>
+                    <Link>DO NOT REMEMBER YOUR PASSWORD ?</Link>
+                    <Link>CREATE A NEW ACCOUNT</Link>
+                </Form>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+            rgba(255, 255, 255, 0.5),
+            rgba(255, 255, 255, 0.5)
+        ),
+        url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+            center;
+`;
+
+const Wrapper = styled.div`
+    width: 30%;
+    padding: 20px;
+    background-color: white;
+`;
+
+const Title = styled.h1`
+    display: flex;
+    font-size: 28px;
+    font-weight: 250;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    padding: 10px;
+    margin: 20px 10px 0px 0px;
+`;
+
+const Agreement = styled.span`
+    font-size: 14px;
+    margin: 20px 0px;
+`;
+
+const Button = styled.button`
+    width: 35%;
+    border: none;
+    padding: 15px 20px;
+    cursor: pointer;
+    background-color: teal;
+    color: white;
+
+    &:hover {
+        background-color: #198d8d;
+        color: white;
+    }
+`;
+
+const Register = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>CREATE AN ACCOUNT</Title>
+                <Form>
+                    <Input placeholder="name" />
+                    <Input placeholder="surname" />
+                    <Input placeholder="username" />
+                    <Input placeholder="email" />
+                    <Input placeholder="password" />
+                    <Input placeholder="confirm password" />
+                    <Agreement>
+                        By creating an account, I am agreeing to all the
+                        <b> Terms & Conditions </b>
+                    </Agreement>
+                    <Button>CREATE</Button>
+                </Form>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Register;
